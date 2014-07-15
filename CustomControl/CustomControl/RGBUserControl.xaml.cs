a2b8dd903926c0d1b2991b1c60b213de662ec213
John <635253084@qq.com>
2014-07-15 16:09:28 +0800

    尝试自定义控件（扩张Button）

---
+++ CustomControl/CustomControl/RGBUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomControl
+{
+	/// <summary>
+	/// Interaction logic for RGBUserControl.xaml
+	/// </summary>
+	public partial class RGBUserControl : UserControl
+	{
+		public RGBUserControl()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
