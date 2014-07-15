de026604fd0d73507ed32cb1891593f470464079
John <635253084@qq.com>
2014-07-15 16:09:28 +0800

    尝试自定义控件（扩张Button）

---
+++ CustomControl/CustomControl/ImageButton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Net;
+
+namespace CustomControl
+{
+	[TemplatePart(Name = "PART_Image", Type = typeof(Image))]
+	public class ImageButton : Button
+	{
+		#region Source
+		public ImageSource Source
+		{
+			get { return (ImageSource)GetValue(SourceProperty); }
+			set { SetValue(SourceProperty, value); }
+		}
+
+		public static readonly DependencyProperty SourceProperty =
+			DependencyProperty.Register("Source", typeof(ImageSource), typeof(ImageButton), new PropertyMetadata((sender, e) =>
+			{
+				var vm = sender as ImageButton;
+				if (vm == null) return;
+			}));
+
+		#endregion
+		static ImageButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageButton), new FrameworkPropertyMetadata(typeof(ImageButton)));
+		}
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+			var image = GetTemplateChild("PART_Image") as Image;
+			if (image != null)
+				image.Visibility = Visibility.Collapsed;
+
+		}
+	}
+}
